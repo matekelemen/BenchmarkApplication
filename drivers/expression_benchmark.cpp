98e7e2f633589e30b146d090d0d957bc31d1fb64
matekelemen <kelemen.mate.12@gmail.com>
2023-05-30 14:44:27 +0200

    add benchmark for expressions

---
+++ drivers/expression_benchmark.cpp
@@ -0,0 +1,37 @@
+// --- External Includes ---
+#include "benchmark/benchmark.h"
+
+// --- Benchmark Includes ---
+#include "custom_utilities/common.hpp"
+
+// --- Core Includes ---
+#include "containers/container_expression/specialized_container_expression.h"
+#include "containers/container_expression/container_data_io.h"
+#include "containers/container_expression/container_expression.h"
+
+
+void LiteralFlatExpression(benchmark::State& rState)
+{
+    // Generate model to read from
+    Kratos::Model& r_model = Kratos::ModelFactory::GetModel();
+    Kratos::ModelPart& r_model_part = r_model.GetModelPart("main");
+
+    // Create a literal expression
+    using NodeExpression = Kratos::SpecializedContainerExpression<
+        Kratos::ModelPart::NodesContainerType,
+        Kratos::ContainerDataIO<Kratos::ContainerDataIOTags::Historical>
+    >;
+    NodeExpression expression(r_model_part);
+    expression.Read(Kratos::DISPLACEMENT);
+
+    for (auto dummy : rState) {
+        expression.Evaluate(Kratos::DISPLACEMENT);
+        benchmark::DoNotOptimize(r_model_part);
+    }
+}
+
+
+BENCHMARK(LiteralFlatExpression);
+
+
+BENCHMARK_MAIN();
