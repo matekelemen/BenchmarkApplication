c1523557b9a7ca7505312d2e0f5083d02f5f9503
matekelemen <kelemen.mate.12@gmail.com>
2023-09-23 18:20:52 +0200

    add benchmark for med loader

---
+++ custom_utilities/cmake_variables.hpp
@@ -0,0 +1,15 @@
+/// @author Máté Kelemen
+
+#pragma once
+
+// --- STL Includes ---
+#include <filesystem>
+
+
+namespace Kratos {
+
+
+std::filesystem::path GetBenchmarkDataDir();
+
+
+} // namespace Kratos
---
+++ drivers/med_benchmark.cpp
@@ -0,0 +1,40 @@
+/// @author Máté Kelemen
+
+// --- External Includes ---
+#include "benchmark/benchmark.h"
+
+// --- Benchmark Includes ---
+#include "custom_utilities/cmake_variables.hpp"
+
+// --- Med Includes ---
+#include "custom_io/med_model_part_io.h"
+
+// --- Core Includes ---
+#include "containers/model.h"
+#include "includes/model_part.h"
+#include "includes/kratos_application.h"
+
+// --- STL Includes ---
+#include <filesystem>
+
+
+void LoadMedFresh(benchmark::State& rState)
+{
+    // Initialize kratos
+    Kratos::KratosApplication kratos("core");
+    kratos.Register();
+
+    for ([[maybe_unused]] auto dummy : rState) {
+        rState.PauseTiming();
+        Kratos::Model model;
+        auto& r_model_part = model.CreateModelPart("root");
+        rState.ResumeTiming();
+
+        Kratos::MedModelPartIO((Kratos::GetBenchmarkDataDir() / "cube_quadratic_tets.med").string()).ReadModelPart(r_model_part);
+        benchmark::DoNotOptimize(r_model_part);
+    }
+}
+
+BENCHMARK(LoadMedFresh);
+
+BENCHMARK_MAIN();
